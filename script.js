d09e1846180f930bac63c05280ccf0f92102586c
Karim Gomez <55936@VNSNY.ORG>
2021-06-15 07:29:45 -0400

    updated html file

---
+++ script.js
@@ -0,0 +1,21 @@
+var filterInput = document.getElementById('filter-Input');
+
+filterInput.addEventListener('keyup', filterNames);
+
+function filterNames () {
+    let filterValue = document.getElementById('filter-Input').value.toUpperCase()
+
+    let names = document.getElementById('collection-with-header')
+
+    let nameItems = ul.querySelectAll('li.collection-item')
+
+    for (let i = 0; i < nameItems.length; i++) {
+        let a = nameItems[i].getElementsByTagName('a')[0]
+        if (a.innerHTML.toUpperCase().indexOf(filterValue) > - 1) {
+            nameItems[i].style.display = ''
+        } else {
+            nameItems[i].style.display = 'none'
+        }
+    }
+}
+
